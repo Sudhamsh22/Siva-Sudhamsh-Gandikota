0c251a09075e457954c2696b9615dc16a9764bcc
Siva Sudhamsh <sivasudhamsh2005@gmail.com>
2026-02-15 15:50:32 +0000

    https://image2url.com/r2/default/images/1771170565295-26841e0b-607e-477d

--- next.config.ts
+++ next.config.ts
@@ -33,6 +33,12 @@ const nextConfig: NextConfig = {
         hostname: 'storage.googleapis.com',
         port: '',
         pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'image2url.com',
+        port: '',
+        pathname: '/**',
       }
     ],
   },
